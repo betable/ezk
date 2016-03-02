fb8287c945cc7754f750a28dd837f606b2476d38
Mariano Cano <mariano@betable.com>
2016-03-01 16:47:58 -0800

    Add tests with cleaners.

--- recipes/lock_test.go
+++ recipes/lock_test.go
@@ -69,6 +69,48 @@ func TestLock(t *testing.T) {
 	}
 }
 
+func TestLockWithCleaner(t *testing.T) {
+	defer testClient.DeleteNodeRecursively("testlock")
+
+	if err := testClient.CreateDir("testlock", acl); err != nil {
+		t.Fatal(err)
+	}
+
+	l1 := NewLock(testClient, "testlock", acl)
+	l2 := NewLock(testClient, "testlock", acl)
+	l2.WithCleaner(100 * time.Millisecond)
+
+	ch := make(chan int, 3)
+
+	// Two locks
+	if err := l1.Lock(); err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	go func() {
+		if err := l2.Lock(); err != nil {
+			t.Fatal(err)
+		}
+		ch <- 1
+		if err := l2.Unlock(); err != nil {
+			t.Fatal(err)
+		}
+		ch <- 2
+	}()
+
+	if x := <-ch; x != 1 {
+		t.Fatalf("Expected 1 instead of %d", x)
+	}
+	if x := <-ch; x != 2 {
+		t.Fatalf("Expected 2 instead of %d", x)
+	}
+	if err := l1.Unlock(); err != zk.ErrNoNode {
+		t.Fatal("Expected zk.ErrNoNode instead of %v", err)
+	}
+}
+
 func TestTryLock(t *testing.T) {
 	defer testClient.DeleteNodeRecursively("testlock")
 
@@ -102,11 +144,47 @@ func TestTryLock(t *testing.T) {
 	}()
 
 	time.Sleep(time.Millisecond * 100)
+
 	if err := l1.Unlock(); err != nil {
 		t.Fatal(err)
 	}
 	// Wait for goroutine
 	<-ch
+}
+
+func TestTryLockWithCleaner(t *testing.T) {
+	defer testClient.DeleteNodeRecursively("testlock")
+
+	if err := testClient.CreateDir("testlock", acl); err != nil {
+		t.Fatal(err)
+	}
+
+	l1 := NewLock(testClient, "testlock", acl)
+	l2 := NewLock(testClient, "testlock", acl)
+	l2.WithCleaner(100 * time.Millisecond)
+
+	ch := make(chan struct{})
+
+	// Two locks
+	if err := l1.TryLock(); err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(time.Millisecond * 100)
+
+	go func() {
+		if err := l2.TryLock(); err != nil {
+			t.Fatalf("Expected nil instead of %v", err)
+		}
+		ch <- struct{}{}
+	}()
+
+	// Wait for goroutine
+	<-ch
+
+	if err := l1.Unlock(); err != zk.ErrNoNode {
+		t.Fatal("Expected zk.ErrNoNode instead of %v", err)
+	}
 }
 
 func TestMain(m *testing.M) {
